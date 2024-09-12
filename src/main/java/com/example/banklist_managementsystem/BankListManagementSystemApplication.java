a4b4c620b76be754c39ff49a057a1f6937a32257
beiken <3456336098@qq.com>
2024-09-12 15:06:45 +0800

    版本控制

---
+++ src/main/java/com/example/banklist_managementsystem/BankListManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.banklist_managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankListManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankListManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/banklist_managementsystem/BankListManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banklist_managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankListManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
